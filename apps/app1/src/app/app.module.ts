7e79126ba45ad3135966366b806fe46a6eaf91ed
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-04-14 16:02:46 +0200

    Add shared header

--- apps/app1/src/app/app.module.ts
+++ apps/app1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { appRoutes } from "./app.routes";
 import { NxWelcomeComponent } from "./nx-welcome.component";
+import { SharedLibraryModule } from "@monorepo-apps/shared-library";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    SharedLibraryModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
   ],
   providers: [],
--- libs/shared-library/src/lib/shared-library.module.ts
+++ libs/shared-library/src/lib/shared-library.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    RouterModule
+  ],
   declarations: [HeaderComponent],
+  exports: [
+    HeaderComponent
+  ]
 })
 export class SharedLibraryModule {}
